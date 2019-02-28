230d7849f8d118ef6304e0aaff3dea18a0a1452d
David Veszelovszki <veszelovszki@gmail.com>
2019-03-01 00:19:54 +0100

    Starting from scratch, without Serverless, with a more traditional setup.

--- backend/lib/utils/cors.js
+++
@@ -1,7 +0,0 @@
-/* cors header settings */
-module.exports = {
-    // Required for CORS support to work
-    'Access-Control-Allow-Origin': '*',
-    // Required for cookies, authorization headers with HTTPS
-    'Access-Control-Allow-Credentials': true,
-};
